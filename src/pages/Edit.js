c23872ad22db53c7c0cce83b2efd4503ed73393c
Pingu <tlsql665@naver.com>
2022-08-29 21:38:29 +0900

    프로젝트 기본 셋팅
    폰트, 레이아웃, 이미지에셋, 공통 컴포넌트 셋팅

---
+++ src/pages/Edit.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id :", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode: ", mode);
+  return (
+    <div>
+      <h1>Edit</h1>
+      <p>이곳은 일기 수정 페이지입니다.</p>
+      <button onClick={() => setSearchParams({ who: "pingu" })}>
+        QS 바꾸기
+      </button>
+
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        HOME으로 가기
+      </button>
+    </div>
+  );
+};
+
+export default Edit;
